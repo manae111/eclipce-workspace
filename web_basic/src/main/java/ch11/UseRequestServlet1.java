66d61e8a6c0b36e095092996c57fb5d3de9e01e4
Sakai <manae.sakai@rakus-partners.co.jp>
2024-01-27 08:34:31 +0900

    first commit

---
+++ web_basic/src/main/java/ch11/UseRequestServlet1.java
@@ -0,0 +1,18 @@
+package ch11;
+
+import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UseRequestServlet1 extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+
+		//リクエストスコープへの登録
+		request.setAttribute("bookTitle", "JSP/サーブレットの本");
+		request.getRequestDispatcher("/view/ch11/useRequest1.jsp").forward(request, response);
+	}
+} 
